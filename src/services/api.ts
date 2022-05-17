b36e072e3179ca6235d03a14c362b83bb5661cea
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-05-16 22:48:37 -0300

    devCommit

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue: { onSuccess: (token: string) => void; onFailure: (error: AxiosError<unknown, any>) => void; }[] = [];
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -18,26 +20,53 @@ api.interceptors.response.use(response => {
       cookies = parseCookies();
 
       const { 'nextauth.refreshToken': refreshToken } = cookies;
-    
-      api.post('/refresh', {
-        refreshToken,
-      }).then(response => {
-        const { token } = response.data;
+      const originalConfig = error.config;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+
+        api.post('/refresh', {
+          refreshToken,
+        }).then(response => {
+          const { token } = response.data;
+          
+          setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/', // Qualquer endereço da aplicação vai ter acesso 
+          });
+  
+          setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/', // Qualquer endereço da aplicação vai ter acesso 
+          });
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
         
-        setCookie(undefined, 'nextauth.token', token, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days
-          path: '/', // Qualquer endereço da aplicação vai ter acesso 
+          failedRequestsQueue.forEach(request => request.onSuccess(token));
+          failedRequestsQueue = [];
+        }).catch(error => {
+          failedRequestsQueue.forEach(request => request.onFailure(error));
+          failedRequestsQueue = [];
+        }).finally(() => {
+          isRefreshing = false;
         });
+      }
 
-        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days
-          path: '/', // Qualquer endereço da aplicação vai ter acesso 
-        });
-
-        api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      // Unica forma de mandar um metodo asyncrono
+      return new Promise((resolve, reject) => {
+        failedRequestsQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`
+          
+            resolve(api(originalConfig))
+          },
+          onFailure: (error: AxiosError) => {
+            reject(error);
+          } 
+        })
       })
     } else {
-      // deslogar o usuário
+      // Deslogar o usuário
     }
   }
 });
