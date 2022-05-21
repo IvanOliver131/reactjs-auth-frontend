1d120b05854a8631c9fec421533f5485158d9a76
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-05-21 17:47:26 -0300

    devCommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from '../contexts/AuthContext';
 import styles from './home.module.css';
@@ -27,4 +29,23 @@ export default function Home() {
       <button type="submit" className={styles.loginButton}>Entrar</button> 
     </form>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  // Por ser do lado do servidor eu possuo o contexto
+  const cookies = parseCookies(ctx);
+  
+  // Se eu possuo a autenticação eu redireciono diretamente para a tela do dashboard
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
