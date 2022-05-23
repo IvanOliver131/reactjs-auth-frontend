cba27381e77fa3355b12a54196a8f6aae708e5da
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-05-23 20:03:58 -0300

    devCommit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../contexts/AuthContext';
 
+// Estilização global
 import '../../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
